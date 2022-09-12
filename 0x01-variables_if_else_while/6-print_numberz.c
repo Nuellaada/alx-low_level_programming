cfa1bffe87e30c2bd66030b34db267d20f08280a
Simon Emmanuella <adaezesimon.sa@gmail.com>
2022-09-12 12:51:25 -0700

    my commit 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		/*convert num to ASCII representation*/
+		putchar(num + '0');
+		++num;
+	}
+	putchar('\n');
+
+	return (0);
+}

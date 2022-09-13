ab5cd88cdb5068969b3a69ba4610aded80275965
Simon Emmanuella <adaezesimon.sa@gmail.com>
2022-09-13 08:13:11 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = '_putchar';
+	putchar(c);
+	putchar('\n');
+	return (0);
+}

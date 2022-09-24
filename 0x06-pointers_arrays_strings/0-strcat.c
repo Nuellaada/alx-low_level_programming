667b2533becffcc372db70bdce3d12cb176605e7
Simon Emmanuella <adaezesimon.sa@gmail.com>
2022-09-23 18:44:05 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - function appends the src string to the dest string
+ * overwriting the terminating null byte (\0) at the end of dest,
+ * and then adds a terminating null byte
+ * Return: pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+
+	_strcat(s1, s2);
+	_putchar('s1');
+	_putchar('\n');
+}

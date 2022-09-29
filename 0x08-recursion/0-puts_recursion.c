20a8478b94758047b2d84ed9ef0dbb2f120dee7a
Simon Emmanuella <adaezesimon.sa@gmail.com>
2022-09-29 10:51:39 -0700

    my initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main - prints a string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	_puts_recursion("puts with recursion");
+	return (0);
+}
